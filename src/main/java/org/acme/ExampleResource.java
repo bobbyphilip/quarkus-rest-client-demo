e0ecfd058dbd58585d80cbeacb05158a4074a8b7
bobbyphilip <bobbyphilip@gmail.com>
2020-05-05 10:43:00 +0200

    Update ExampleResource.java

--- src/main/java/org/acme/ExampleResource.java
+++ src/main/java/org/acme/ExampleResource.java
@@ -1,16 +1,30 @@
 package org.acme;
 
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 @Path("/hello")
 public class ExampleResource {
 
-    @GET
+    @Inject
+    @RestClient
+    MailService mailService;
+
     @Produces(MediaType.TEXT_PLAIN)
+    @GET
     public String hello() {
+        System.out.print("In the API");
+        JsonObject key = Json.createObjectBuilder().add("key", "ABCD").build();
+        System.out.println("The json built is "+key);
+        JsonObject response = mailService.ping(key);
+        System.out.println("The response is " + response);
         return "hello";
     }
 }
